c1fe9ad2f8de5c46643eabbcbf5d4d8cde2d879a
Jimmy <1037892796@qq.com>
2024-02-02 12:42:58 +0800

    短信验证码发送+短信验证码登录

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -32,12 +32,11 @@ public class UserController {
     private IUserInfoService userInfoService;
 
     /**
-     * 发送手机验证码1
+     * 发送手机验证码
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-
-        return Result.fail("功能未完成");
+        return userService.sendCode(phone,session);
     }
 
     /**
@@ -46,8 +45,7 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
-        // TODO 实现登录功能
-        return Result.fail("功能未完成");
+        return userService.login(loginForm,session);
     }
 
     /**
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -14,7 +23,44 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+       if(!RegexUtils.isPhoneInvalid(phone)) {
+           return Result.fail("手机号格式错误");
+       }
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute("code",code);
+        log.debug("验证码为：{}",code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        if(!RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+            return Result.fail("手机号格式错误");
+        }
+        Object cachecode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if(cachecode == null || ! cachecode.toString().equals(code)){
+            return Result.fail("验证码错误");
+        }
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        if (user == null){
+            user = createUserWithPhone (loginForm.getPhone());
+        }
+        session.setAttribute("user",user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone){
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
