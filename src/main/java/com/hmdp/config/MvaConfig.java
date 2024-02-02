0b2d681d55e704e8a54de234026df20278aaa4be
Jimmy <1037892796@qq.com>
2024-02-02 16:55:03 +0800

    利用Redis替代sessionID登录

--- src/main/java/com/hmdp/config/MvaConfig.java
+++ src/main/java/com/hmdp/config/MvaConfig.java
@@ -1,7 +1,6 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
